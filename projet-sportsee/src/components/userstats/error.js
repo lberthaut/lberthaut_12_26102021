0162d7656677ac8dd7183895c36dca59011002c4
Louis Berthaut <louis.berthaut@gmx.com>
2022-06-21 12:42:27 +0200

    change url

--- projet-sportsee/src/components/userstats/error.js
+++ projet-sportsee/src/components/userstats/error.js
@@ -9,44 +9,29 @@ import propTypes from 'prop-types';
  * @param {dataKey} datakey of the user
  */
 
-export default class Error extends React.Component{
-    constructor(props) {
-        super(props);
-      }
+export default class Error extends React.Component {
 
-    
-      render() {
 
-          return(
-            <div className="errorblock"  key={this.props.key}>
-                <p className="errortitle">Oups...</p>
-                <p className="errortext">Les informations que vous cherchez ne sont pas accessible. Veuillez réessayer ultérieurement</p>
-            </div>
-          )
-      }
+
+  render() {
+
+    return (
+      <div className="errorblock" key={this.props.key}>
+        <p className="errortitle">Oups...</p>
+        <p className="errortext">Les informations que vous cherchez ne sont pas accessible. Veuillez réessayer ultérieurement</p>
+      </div>
+    )
+  }
 }
 
-  /**
- * Check the types of the datas
- * 
- * @Property
- * @this {propTypes}
- */
+/**
+* Check the types of the datas
+* 
+* @Property
+* @this {propTypes}
+*/
 
 
-   Error.propTypes={
-    key:propTypes.string,
-  }
-
-
-
-
-      /* render(){
-        this.props.key="error"
-        return(
-            <div className="errorblock"  key={this.props.key}>
-                <p className="errortitle">Oups...</p>
-                <p className="errortext">Les informations que vous cherchez ne sont pas accessible. Veuillez réessayer ultérieurement</p>
-            </div>
-        )
-    } */
+Error.propTypes = {
+  key: propTypes.string,
+}
