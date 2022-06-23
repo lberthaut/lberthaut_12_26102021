c28a081d74aea1afec2af61d3b1767210390f17a
Louis Berthaut <louis.berthaut@gmx.com>
2022-06-23 16:51:47 +0200

    Revert "try to resolve fetch error"
    This reverts commit 48eaaa762a18c0a14323bfaafbe3506231aaf499.

--- projet-sportsee/src/services/api.js
+++ projet-sportsee/src/services/api.js
@@ -7,31 +7,30 @@
  * @param {URL} URLs of the users' datas
  */
 
-/*disabled for mocked datas */
-
 export default class Usefetch {
+ 
 
-  static async getUser(userId) {
-    return fetch('http://localhost:3000/user/' + userId)
-      .then(response => response.json())
-      .then(data => data.data)
+ static async getUser(userId){
+  return fetch('http://localhost:3000/user/'+ userId)
+  .then(response => response.json())
+  .then(data=>data.data)
   }
 
-  static async getUserActivity(userId) {
-    return fetch('http://localhost:3000/user/' + userId + '/activity')
-      .then(response => response.json())
-      .then(data => data.data)
-  }
+  static async getUserActivity(userId){
+    return fetch('http://localhost:3000/user/'+ userId + '/activity')
+    .then(response => response.json())
+    .then(data=>data.data)
+    }
 
-  static async getUserAverageSessions(userId) {
-    return fetch('http://localhost:3000/user/' + userId + '/average-sessions')
+    static async getUserAverageSessions(userId){
+      return fetch('http://localhost:3000/user/'+ userId + '/average-sessions')
       .then(response => response.json())
-      .then(data => data.data)
-  }
+      .then(data=>data.data)
+      }
 
-  static async getUserPerformance(userId) {
-    return fetch('http://localhost:3000/user/' + userId + '/performance')
-      .then(response => response.json())
-      .then(data => data.data)
-  }
+      static async getUserPerformance(userId){
+        return fetch('http://localhost:3000/user/'+ userId + '/performance')
+        .then(response => response.json())
+        .then(data=>data.data)
+        }
 }
--- projet-sportsee/src/services/mock.js
+++ projet-sportsee/src/services/mock.js
@@ -9,44 +9,36 @@
 
 export default class Usefetch {
   static async getUser(userId) {
-    return fetch("./mockeddatas.json")
-      .then((result) => result.text())
+    return fetch("mockeddatas.json")
+      .then((result) => result.json())
       .then((data) => {
         return data.USER_MAIN_DATA.find((user) => user.id === userId);
-      }).catch((error) => {
-        console.log(error);
       });
   }
 
   static async getUserActivity(userId) {
-    return fetch("./mockeddatas.json")
+    return fetch("mockeddatas.json")
       .then((result) => result.json())
       .then((data) => {
         return data.USER_ACTIVITY.find((user) => user.userId === userId);
-      }).catch((error) => {
-        console.log(error);
       });
   }
 
   static async getUserAverageSessions(userId) {
-    return fetch("./mockeddatas.json")
+    return fetch("mockeddatas.json")
       .then((result) => result.json())
       .then((data) => {
         return data.USER_AVERAGE_SESSIONS.find((user) => user.userId === userId);
-      }).catch((error) => {
-        console.log(error);
       });
   }
 
   static async getUserPerformance(userId) {
-    return fetch("./mockeddatas.json")
+    return fetch("mockeddatas.json")
       .then((result) => result.json())
       .then((data) => {
         return data.USER_PERFORMANCE.find((user) => user.userId === userId);
-      }).catch((error) => {
-        console.log(error);
       });
   }
 }
 
-/* file:///C:/Users/louis/OneDrive/Bureau/MOOC/P12/projet-sportsee/projetsportsee/projetsportsee/projet-sportsee/public/mockeddatas.json */
+/*test commit */
